3a615fdc35a1c40455acd802bfd0ad8a2775478a
Gonzalo Catrilef <catrilef.gonzalo@icloud.com>
2023-07-23 12:58:12 -0400

    Header terminado

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+const carrito= []
+
+const quesos = [ {id:1, nombre: 'AMERICAN CHEESE', precio: 9950, origen: 'Inglaterra'},
+                 {id:2, nombre:'BLUE CHEESE', precio:18990, origen:'Francia'},
+                 {id:3, nombre:'BRIE CHEESE', precio:19990, origen:'Francia'},
+                 {id:4, nombre:'CAMBERT CHEESE', precio:15990, origen:'Francia'},
+                 {id:5, nombre:'CHEEDAR CHEESE', precio:7980,origen:'Inglaterra'},
+                 {id:6, nombre:'EDAM CHEESE', precio:15990, origen:'Países Bajos'},
+                 {id:7, nombre:'GOUDA CHEESE', precio:5990, origen:'Países Bajos'},
+                 {id:8, nombre:'FETA CHEESE', precio:12990, origen:'Grecia'}]
